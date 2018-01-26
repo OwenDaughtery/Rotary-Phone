4216be87bc397fc5fa31d0978f6783dc78a1d31b
OwenDaughtery <OwenJ.daughtery@gmail.com>
2018-01-26 21:01:26 +0000

    Addition of "pickUpTimer" function
    "flattened" the whole code by adding the pickUpTimer function, which continuously tests whether the phone is off or on the hook and acts appropriately.

--- firstCommit.py
+++ firstCommit.py
@@ -8,32 +8,34 @@ def idle():
 
 def phoneRing():
     play("phoneRing.mp3")
-    timer = 0
-    pickedUp = False
-    startTime = time.time()
-    while timer<30 and not pickedUp:
-        pickedUp = receiverPickedUp
-        timer = time.time() - startTime 
-        time.sleep(0.01)
-    stop("phoneRing.mp3")
+    pickedUp = pickUpTimer(30, "phoneRing.mp3", False)
     if pickedUp:
         #Selecting audio file to play
         randomNum = randint(0,3)
         fileToPlay,audioLength = audioFiles[randomNum]
         play(fileToPlay)
-        timer = 0
-        startTime = time.time()
-        #While loop to test whether audio file runs out "organically" or is hung up on
-        while timer<audioLength and pickedUp:
-            timer = time.time() - startTime
-            pickedUp = receiverPickedUp
-            if not pickedUp:
-                stop(fileToPlay)
-            time.sleep(0.01)
+        pickUpTimer(audioLength, fileToPlay, True)
         time.sleep(30)
     else: #Receiver wasn't picked up, time out whole program for 30 seconds
         time.sleep(30)
 
+#pickUpTimer takes 3 parameters,
+#1: How long the timer should last,
+#2: Name of the file that should stop playing
+#3: Whether the while loop should exit if the phone is "off the hook" or not,
+#If the 3rd parameter is False the while loop will be ran as long as the phone hasn't been picked up.
+def pickUpTimer(timerEnd, fileName, offHook):        
+    pickedUp = ReceiverPickedUp()        
+    timer = 0
+    startTime = time.time()
+    while timer<timerEnd and pickedUp == offHook:
+        pickedUp = receiverPickedUp()
+        timer = time.time() - startTime 
+        time.sleep(0.01)
+        if not offHook:
+            stop(fileName) 
+    return timer<timerEnd
+        
 def receiverPickedUp():
     #Code for testing whether phone is off the hook
     return Boolean
